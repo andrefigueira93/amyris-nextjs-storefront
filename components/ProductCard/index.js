be4a0a7ff47475e6b7732794fcbeab75bf79be1f
André Figueira <andre.figueira@envolti.com.br>
2021-10-19 18:49:35 -0300

    Adding price & fix link url

--- components/ProductCard/index.js
+++ components/ProductCard/index.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { priceFormatter } from '../../utils/helpers';
 
 const ProductCard = ({ product }) => {
   const { handle, title } = product;
   const { altText, originalSrc } = product.images.edges[0].node;
+  const price = product.priceRange.minVariantPrice.amount;
   return (
-    <Link href={`/product/${handle}`}>
+    <Link href={`/products/${handle}`}>
       <a className="group" href="">
         <div className="w-full bg-gray-200 rounded-3xl overflow-hidden">
           <div className="relative group-hover:opacity-75 h-72">
@@ -18,6 +20,9 @@ const ProductCard = ({ product }) => {
           </div>
         </div>
         <h3 className="mt-4 text-lg font-medium text-gray-900">{title}</h3>
+        <p className="mt-1 text-sm text-gray-700">
+          {priceFormatter.format(price)}
+        </p>
       </a>
     </Link>
   );
