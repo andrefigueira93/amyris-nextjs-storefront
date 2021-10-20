060898fab50b28c55fc938df2a2c1fe09aa661a5
André Figueira <andre.figueira@envolti.com.br>
2021-10-20 20:04:23 -0300

    MiniCart Adjusts

--- components/MiniCart/index.js
+++ components/MiniCart/index.js
@@ -74,60 +74,68 @@ const MiniCart = ({ cart }) => {
                             role="list"
                             className="-my-6 divide-y divide-gray-200"
                           >
-                            {cart.map((product) => (
-                              <li key={product.id} className="py-6 flex">
-                                <div className="relative flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
-                                  <Image
-                                    src={product.image}
-                                    alt={product.title}
-                                    layout="fill"
-                                    objectFit="cover"
-                                  />
-                                </div>
+                            {cart.map(
+                              ({
+                                id,
+                                image,
+                                title,
+                                handle,
+                                variantPrice,
+                                variantQuantity,
+                                variantTitle,
+                              }) => (
+                                <li key={id} className="py-6 flex">
+                                  <div className="relative flex-shrink-0 w-24 h-24 border border-gray-200 rounded-md overflow-hidden">
+                                    <Image
+                                      src={image}
+                                      alt={title}
+                                      layout="fill"
+                                      objectFit="cover"
+                                    />
+                                  </div>
 
-                                <div className="ml-4 flex-1 flex flex-col">
-                                  <div>
-                                    <div className="flex justify-between text-base font-medium text-gray-900">
-                                      <h3>
-                                        <Link
-                                          href={`/products/${product.handle}`}
-                                          passHref
-                                        >
-                                          <a onClick={() => setCartOpen(false)}>
-                                            {product.title}
-                                          </a>
-                                        </Link>
-                                      </h3>
-                                      <p className="ml-4">
-                                        {priceFormatter.format(
-                                          product.variantPrice
-                                        )}
+                                  <div className="ml-4 flex-1 flex flex-col">
+                                    <div>
+                                      <div className="flex justify-between text-base font-medium text-gray-900">
+                                        <h3>
+                                          <Link
+                                            href={`/products/${handle}`}
+                                            passHref
+                                          >
+                                            <a
+                                              onClick={() => setCartOpen(false)}
+                                            >
+                                              {title}
+                                            </a>
+                                          </Link>
+                                        </h3>
+                                        <p className="ml-4">
+                                          {priceFormatter.format(variantPrice)}
+                                        </p>
+                                      </div>
+                                      <p className="mt-1 text-sm text-gray-500">
+                                        {variantTitle}
                                       </p>
                                     </div>
-                                    <p className="mt-1 text-sm text-gray-500">
-                                      {product.variantTitle}
-                                    </p>
-                                  </div>
-                                  <div className="flex-1 flex items-end justify-between text-sm">
-                                    <p className="text-gray-500">
-                                      Qty {product.variantQuantity}
-                                    </p>
+                                    <div className="flex-1 flex items-end justify-between text-sm">
+                                      <p className="text-gray-500">
+                                        Qty {variantQuantity}
+                                      </p>
 
-                                    <div className="flex">
-                                      <button
-                                        onClick={() =>
-                                          removeCartItem(product.id)
-                                        }
-                                        type="button"
-                                        className="font-medium text-gray-500 hover:text-gray-800"
-                                      >
-                                        Remove
-                                      </button>
+                                      <div className="flex">
+                                        <button
+                                          onClick={() => removeCartItem(id)}
+                                          type="button"
+                                          className="font-medium text-gray-500 hover:text-gray-800"
+                                        >
+                                          Remove
+                                        </button>
+                                      </div>
                                     </div>
                                   </div>
-                                </div>
-                              </li>
-                            ))}
+                                </li>
+                              )
+                            )}
                           </ul>
                         ) : (
                           <div>
